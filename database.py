a6e82aca6f76844bf59715acddc27e1ee430e168
Samuel Agbedia <solumide83@gmail.com>
2024-10-13 06:11:25 +0800

    implementing some database stuff

---
+++ database.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import Session
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
